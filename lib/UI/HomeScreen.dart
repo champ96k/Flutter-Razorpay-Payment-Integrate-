3309caa3977277089cbb27785535fc03bf743bc9
Tushar <champ96k@gmil.com>
2020-06-09 01:40:09 +0530

    final commit

--- lib/UI/HomeScreen.dart
+++ lib/UI/HomeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,11 +8,134 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Razorpay razorpay;
+
+  TextEditingController _amountController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    razorpay = Razorpay();
+    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    razorpay.clear();
+  }
+
+  void checkOption() {
+    var options = {
+      "key": "rzp_test_Mw2gPeWBSCWUCR", // enter razorpay key here
+      "amount": num.parse(_amountController.text) * 100,
+      'name': "Tushar Nikam",
+      'description': "to buy cup Of Tea for champ ",
+      'prefill': {
+        "contact": '',
+        "email": '',
+      },
+      'external': {
+        "wallets": ['payment'],
+      },
+    };
+
+    try {
+      razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    // Do something when payment succeeds
+    Fluttertoast.showToast(
+        msg: "Payment Success",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    // Do something when payment fails
+    Fluttertoast.showToast(
+        msg: "Payment Error",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    // Do something when an external wallet was selected
+    Fluttertoast.showToast(
+        msg: "External Wallet",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.indigo,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Razorpay Payment"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+            SizedBox(
+              height: 16.0,
+            ),
+
+
+            //Amount Text Field
+            TextField(
+              textAlign: TextAlign.center,
+                    controller: _amountController,
+                    keyboardType: TextInputType.numberWithOptions(
+                      signed: true,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: "Enter amount", 
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0)
+                      )
+                    )
+                  ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+
+            //Pay Button
+            RaisedButton(
+              onPressed: () {
+                checkOption();
+                _amountController.clear();
+              },
+              color: Colors.red,
+              child: Text(
+                "Pay Now",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
